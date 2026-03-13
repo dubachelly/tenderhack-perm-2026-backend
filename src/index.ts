9546c03f9961f209486d31bc6475e4922b083b7e
Artem Netsvetaev <cobal2m@gmail.com>
2026-03-14 00:18:29 +0500

    добавили сваггер

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import "dotenv/config";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import { swaggerDocument } from "./swagger";
 import steRouter from "./routes/ste";
 import contractsRouter from "./routes/contracts";
 import searchRouter from "./routes/search";
@@ -13,10 +15,13 @@ app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/ste", steRouter);
 app.use("/contracts", contractsRouter);
 app.use("/search", searchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Swagger UI:    http://localhost:${PORT}/docs`);
 });
